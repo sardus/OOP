8da565ff7e2bdf1860d101933395b3499f9a2774
Andrei Shmelev <50106717+sardus@users.noreply.github.com>
2021-06-15 22:48:32 +0300

    New mark

--- src/com/javalesson/oop/Main.java
+++ src/com/javalesson/oop/Main.java
@@ -2,23 +2,23 @@ package com.javalesson.oop;
 
 public class Main {
     public static void main(String[] args) {
-        
+
         System.out.println("Dog's count " + Dog.getDogsCount());
 
-        Dog lab = new Dog();
-        lab.setName("Alex");
-        lab.setPaws(4);
-        lab.setTail(1);
-        lab.setBreed("Labrador");
-        lab.setSize("Average");
-        lab.bite();
+        Dog labrador = new Dog();
+        labrador.setName("Alex");
+        labrador.setPaws(4);
+        labrador.setTail(1);
+        labrador.setBreed("Labrador");
+        labrador.setSize("Big");
+        labrador.bite();
 
         Dog pickiness = new Dog();
         pickiness.setName("Alex");
         pickiness.setPaws(4);
         pickiness.setTail(1);
         pickiness.setBreed("Labrador");
-        pickiness.setSize("Average");
+        pickiness.setSize("Small");
         pickiness.bite();
 
         Dog doberman = new Dog();
